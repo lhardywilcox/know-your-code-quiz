a28570e99fdeb8b3fca09e512956a6ed409cdeeb
LHardyWilcox <motacycaryda@mac.com>
2023-01-20 14:40:07 -0800

    code updates

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,17 @@
 var startBtn = document.querySelector(".start");
+var highScoreLink = document.querySelector("#high-score-link");
 var secondsLeft = 60;
 var questionNumberIdx = 0;
 var questionCard = document.querySelector("#q1");
 var score = 0;
+var initials = document.querySelector(".initials");
 var submitBtn = document.querySelector(".submitbtn");
 var goBackBtn = document.querySelector(".goback");
 var clearScoreBtn = document.querySelector(".clear");
 var timeEl = document.querySelector(".timer");
 var titleCard = document.querySelector("#title-card");
 var scoreCard = document.querySelector("#score");
+var highScoreCard = document.querySelector(".high-score-card");
 var userScore = document.querySelector("#user-score");
 
 var questionEl = document.querySelector(".question");
@@ -19,8 +22,7 @@ var answer4El = document.querySelector(".answer-4");
 
 var correctOrWrong = document.querySelector(".correctOrWrong");
 
-//                   Question     option1 option2
-const question1 = ["What is 2+2?", "4", "6", "0", "1"];
+var highScoreList = [];
 
 // Array of Question Objects
 const questions = [
@@ -68,8 +70,8 @@ function startTimer() {
         if (secondsLeft === 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
-            // Calls function to end game
-            endGame();
+            // Calls function to score question
+            scoreQuestion();
         }
     }, 1000);
 }
@@ -109,38 +111,72 @@ function showNextQuestion(option) {
     }
     // Increment the index of the questions
     questionNumberIdx++;
-
     showQuestion();
-
     setTimeout(function () {
         correctOrWrong.innerHTML = "";
     }, 1000);
 }
 
 function scoreQuestion() {
+    timeEl.style.display = "none";
     questionCard.style.display = "none";
     scoreCard.style.display = "block";
     userScore.innerHTML = "Your final score is: " + score;
-
-    // endGame();
+    endGame();
 }
 
 function endGame() {
 
     // input initials
     // save initials and score to local storage
+    submitBtn.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        var newScoreEntry = {
+            initials: initials.value,
+            score: score
+        };
+
+        highScoreList.push(newScoreEntry);
+
+        console.log(highScoreList);
+
+        localStorage.setItem("highScoreList", JSON.stringify(highScoreList));
+    });
+
+    console.log("Ending game!");
+
+    // reset browser window after 10 second delay
+
+    setTimeout(() => {
+        document.location.reload();
+    }, 10000);
 }
 
-function highScore() {
+highScoreLink.addEventListener("click", function () {
+    showHighScores();
+});
+
+function showHighScores() {
+    titleCard.style.display = "none";
+    highScoreCard.style.display = "block";
+
     // get list of scores from local storage
-    // goBackBtn.addEventListener("click", goBack());
-    // clearScoresBtn.addEventListener("click", clearScores());
+    JSON.parse(localStorage.getItem("highScoreList"));
+
+    if (highScoreList !== null) {
+        document.querySelector("#high-scores").textContent = highScoreList.initials + " - " + highScoreList.score
+    };
+    console.log("See the scores?");
 }
 
-function goBack() {
-    // reset browser page
-}
+goBackBtn.addEventListener("click", function () {
+    document.location.reload();
+});
 
-function clearScores() {
-    // reset local storage
-}
+clearScoreBtn.addEventListener("click", function () {
+    localStorage.clear();
+    console.log("All clear!");
+    document.location.reload();
+});
+
